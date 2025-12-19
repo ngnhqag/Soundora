d02bc757102f601fdbf447beb9f2f89593d81a93
ngnhqag <nhatquanglgbg@gmail.com>
2025-12-19 18:01:18 +0700

    feat(player): Configure ExoPlayer buffer

--- app/src/main/java/com/soundlab/soundora/player/PlayerControllerImpl.kt
+++ app/src/main/java/com/soundlab/soundora/player/PlayerControllerImpl.kt
@@ -2,20 +2,34 @@ package com.soundlab.soundora.player
 
 import android.content.Context
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
 import com.soundlab.soundora.domain.model.Track
 
+@UnstableApi
 class PlayerControllerImpl(
     context: Context
 ): PlayerController {
 
-    private val exoPlayer = ExoPlayer.Builder(context).build()
+
+    private val loadControl = DefaultLoadControl.Builder()
+        .setBufferDurationsMs(
+            15_000,
+            30_000,
+            5_000,
+            10_000
+        )
+        .build()
+    private val exoPlayer = ExoPlayer.Builder(context)
+        .setLoadControl(loadControl)
+        .build()
 
     override fun play(track: Track) {
         val mediaItem = MediaItem.fromUri(track.preview)
         exoPlayer.setMediaItem(mediaItem)
         exoPlayer.prepare()
-        exoPlayer.play()
+        exoPlayer.playWhenReady = true
     }
 
     override fun resume() {
